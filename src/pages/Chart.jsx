7f888a8f9d6d9f06af9f041b6f9c70957b5d7666
debtx <sankad@slt.com.lk>
2025-02-24 11:22:20 +0530

    update

--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -1,3 +1,5 @@
+//dummy
+
 import { useState, useRef, useEffect } from "react";
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from "chart.js";
